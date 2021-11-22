c68a8665abfdb7872c3fff0baf479c2912ad68a2
SharapaGorg <sharapov.savely@yandex.ru>
2021-11-22 23:22:25 +0300

    fucking shit suck dick fucking css and nuxt

--- components/DynamicNavigator.vue
+++ components/DynamicNavigator.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class = 'navigator' ref = 'navigator'></div>
+  <div ref='container'>
+    <div class='navigator' ref='navigator'></div>
+  </div>
 </template>
 
 <script>
@@ -7,34 +9,72 @@ export default {
   name: "DynamicNavigator",
   data() {
     return {
-        activateItems : false
+      activateItems: false,
+      itemsWidthHeight: 15,
+      activeItems: [],
+      isUsed : false,
+      navigatorItems: [
+        {
+          name: 'Profile',
+          path: '/profile'
+        },
+        {
+          name: 'Home',
+          path: '/'
+        }
+      ]
+    }
+  },
+  methods: {
+    moveItems() {
+      // event.stopPropagation() // doesn`t work
+      // event.stopImmediatePropagation() // the same case
+      let indent;
+      let navigator = this.$refs.navigator
+      for (let i = 0; i < this.navigatorItems.length; i++) {
+        let navigatorItem = document.createElement('div');
+        navigatorItem.className = 'navigator-item';
+
+        if (this.isUsed) {
+          navigatorItem = this.activeItems[i]
+          // console.log(this.activeItems)
+        } else {
+          navigator.appendChild(navigatorItem)
+        }
+
+        // let navigatorProps = navigator.getBoundingClientRect()
+
+        switch (i) {
+          case 0:
+            indent = !this.activateItems ? 50 : 0;
+            break
+          case 1:
+            indent = !this.activateItems ? -50 : 0;
+            break
+          default:
+            console.error('IndexError')
+            break
+        }
+
+        // navigatorItem.style.transition = 'all .3s linear';
+
+        navigatorItem.style.transform = `translate(${indent}px, 0px)`;
+
+        navigatorItem.style.left = indent + 'px'
+        this.activeItems.push(navigatorItem)
+      }
+      this.isUsed = true;
+      this.activateItems = !this.activateItems
     }
   },
   mounted() {
-    let navigator = this.$refs.navigator
-
-    navigator.onclick = () => {
-      console.log(1);
+    this.$refs.navigator.onclick = () => {
+        this.moveItems()
     }
   }
 }
 </script>
 
-<style scoped>
-
-.navigator {
-  height: 50px;
-  width: 50px;
-  background-color: #50c878;
-  border-radius: 50%;
-  display: inline-block;
-  box-shadow: 0 3px 2px #574D4D;
-  transition : .5s;
-}
-
-.navigator:hover {
-  box-shadow: 0 10px 15px #574D4D;
-  border: 1px dashed #EE5180;
-}
+<style src='../layouts/default.css'>
 
 </style>
---
+++ layouts/NotFound404.vue
@@ -0,0 +1,19 @@
+<template>
+  <center>
+    <div>
+      <br>
+      <h1> NOT FOUND PIDARAS ERROR</h1>
+      <br>
+    </div>
+  </center>
+</template>
+
+<script>
+export default {
+  name: "NotFound404"
+}
+</script>
+
+<style scoped>
+
+</style>
